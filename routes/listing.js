c2a339ea43163e818e0ce4617b331d06b9b26a64
Prasun Babu <babuprasun790@gmail.com>
2026-09-25 00:18:28 +0530

    Add cookies, Express Session and Flash message support

--- routes/listing.js
+++ routes/listing.js
@@ -35,6 +35,10 @@ router.get("/:id",
     wrapAsync(async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if (!listing) {
+        req.flash("error", "Listing you requested for does not exist!");
+        return res.redirect("/listings");
+    }
     res.render("listings/show.ejs", {listing});
     })
 );
@@ -45,6 +49,7 @@ router.post("/",
     wrapAsync(async (req, res, next) => {
         const newListing = new Listing (req.body.listing);
         await newListing.save();
+        req.flash("success", "New Listing Created!");
         res.redirect("/listings");
     })
 );
@@ -54,6 +59,10 @@ router.get("/:id/edit",
     wrapAsync(async (req, res) => {
         let {id} = req.params;
         const listing = await Listing.findById(id);
+        if (!listing) {
+        req.flash("error", "Listing you requested for does not exist!");
+        return res.redirect("/listings");
+        }
         res.render("listings/edit.ejs", {listing});
     })
 );
@@ -63,6 +72,7 @@ router.put("/:id", validateListing,
     wrapAsync(async (req, res) => {
         let {id} = req.params;
         await Listing.findByIdAndUpdate(id, {...req.body.listing});
+        req.flash("success", "Listing Updated!");
         res.redirect(`/listings/${id}`);
     })
 );
@@ -73,6 +83,7 @@ router.delete("/:id",
         let {id} = req.params;
         let deletedListing = await Listing.findByIdAndDelete(id);
         console.log(deletedListing);
+        req.flash("success", "Listing Deleted!");
         res.redirect("/listings");
     })
 );
